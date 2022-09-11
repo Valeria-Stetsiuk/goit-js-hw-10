3f9f7ad8d3df295b78d78cc51bc2ed1eefb9d066
Valeria <valeriastetsiuk3546@gmail.com>
2022-09-11 22:10:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
 searchBox.addEventListener('input', debounce(onInputBox, DEBOUNCE_DELAY));
 function onInputBox(evt) {}
