160109d5f8d857b64907edb80165529101bc8ae5
Valeria <valeriastetsiuk3546@gmail.com>
2022-09-12 14:30:25 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -9,4 +9,58 @@ const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
 searchBox.addEventListener('input', debounce(onInputBox, DEBOUNCE_DELAY));
-function onInputBox(evt) {}
+function onInputBox(evt) {
+  const Inputvalue = evt.target.value.trim();
+  if (Inputvalue) {
+    fetchCountries(Inputvalue).then(setMark).catch(alertWrong);
+  } else {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+  }
+}
+function setMark(countries) {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  if (countries.length > 10) {
+    alertInfo();
+  } else if (countries.length === 1) {
+    countryInfo.insertAdjacentHTML('beforeend', createCountry(countries));
+    countryList.insertAdjacentHTML('beforeend', createMark(countries));
+  } else {
+    countryList.insertAdjacentHTML('beforeend', createMark(countries));
+  }
+}
+function createMark(arr) {
+  return arr
+    .map(
+      ({ name, flags }) => `
+          <li class="country-item">
+              <img class="country-flag" src="${flags.svg}" alt="flag of ${name.official}" width = 25px >
+              <h2 class="country-name">${name.official}</h2>
+          </li>`
+    )
+    .join('');
+}
+function createCountry(arr) {
+  return arr
+    .map(
+      ({ capital, population, languages }) => `
+    <ul class="country-info__list">
+     <li class="country-capital"><p><b>Capital: </b>${capital}</p></li>
+     <li class="country-population"><p><b>Population: </b>${population}</p></li>
+     <li class="country-languages"><p><b>Languages: </b>${Object.values(
+       languages
+     ).join(', ')}</p></li>
+        </ul>`
+    )
+    .join('');
+}
+
+function alertWrong() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+function alertInfo() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,9 @@
 export function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1/name/';
   const searchCountries = '?fields=flag,capital,name,population,languages';
-  fetch(`${BASE_URL}${name}?${searchCountries}`).then(response => {
+  return fetch(`${BASE_URL}${name}?${searchCountries}`).then(response => {
     if (!response.ok) {
-      throw new Error(response.status);
+      throw new Error(data.status);
     }
     return response.json();
   });
